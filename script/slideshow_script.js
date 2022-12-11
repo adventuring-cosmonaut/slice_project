c8855b7a07a3acdd547f92dbfeafa2f683f0a1e0
CUR53 <114309130+Matias1706@users.noreply.github.com>
2022-12-11 03:39:02 +0100

    Red line effect in the header

--- script/slideshow_script.js
+++ script/slideshow_script.js
@@ -28,3 +28,27 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = 'block';
   dots[slideIndex - 1].className += ' active';
 }
+
+// nav red line effect
+
+// Get the nav and .triangle-top elements
+const nav = document.querySelector('nav');
+const triangleTop = document.querySelector('.triangle-top');
+
+// position of the .triangle-top element
+triangleTop.style.top = '100px';
+
+// Listen for the scroll event
+window.addEventListener('scroll', () => {
+  let scrollTop = window.pageYOffset;
+
+  if (scrollTop > 100) {
+    triangleTop.style.top = '0';
+  } else {
+    triangleTop.style.top = '100px';
+  }
+
+  setTimeout(() => {
+    triangleTop.style.top = '100px';
+  }, 800);
+});
