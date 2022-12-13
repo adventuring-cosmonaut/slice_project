af84f16357c76463eb9530c5b75805c54e6a4cc0
Q - Q <113436291+adventuring-cosmonaut@users.noreply.github.com>
2022-12-13 13:09:57 +0100

    burger menu work

--- script/burg_menu_script.js
+++ script/burg_menu_script.js
@@ -1,4 +1,13 @@
 let burgContent = document.querySelector('#burg_menu_content');
-let burgIcon = document.querySelector('#burg_menu_icon');
+let burgContentOpen = () =>
+  burgContent.classList.contains('burg_content_slide');
 
-const burgSlide = () => 
+let openBurg = () => burgContent.classList.add('burg_content_slide');
+
+let closeBurg = () => burgContent.classList.remove('burg_content_slide');
+
+document
+  .querySelector('#burg_menu_icon')
+  .addEventListener('click', function () {
+    burgContentOpen() ? closeBurg() : openBurg();
+  });
