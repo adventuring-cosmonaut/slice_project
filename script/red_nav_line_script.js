308836f2a630ca08a91357dfb810618bba9c566c
Q - Q <113436291+adventuring-cosmonaut@users.noreply.github.com>
2022-12-14 18:51:53 +0100

    Code cleanup

--- script/red_nav_line_script.js
+++ script/red_nav_line_script.js
@@ -1,13 +1,8 @@
-// nav red line effect
-
-// Get the nav and .triangle-top elements
 const nav = document.querySelector('nav');
 const triangleTop = document.querySelector('.triangle-top');
 
-// position of the .triangle-top element
 triangleTop.style.top = '100px';
 
-// Listen for the scroll event
 window.addEventListener('scroll', () => {
   let scrollTop = window.pageYOffset;
 
